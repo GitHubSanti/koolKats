23acae796ccc7ae152fbcca67d1c0878696e1259
Spiderman <santiagosepulveda@me.com>
2019-01-19 10:57:24 -0500

    first push

---
+++ routes/api-routes.js
@@ -0,0 +1,20 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+
+// Grabbing our models
+
+var db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // POST route for saving a new todo. You can create a todo using the data on req.body
+  app.post("/api/addEvent", function(req, res) {
+
+  });
+};
