21dd32e090bf1d32b69f36e427c25ef02d2a15c3
Santiago Sepulveda <42755547+GitHubSanti@users.noreply.github.com>
2019-01-20 18:51:47 -0500

    modified Routes section (lines 27 & 28)
    now incorporating both html and api calls in server file:
    --require("./routes/html-routes.js")(app);
    --require("./routes/api-routes.js")(app);

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.static("public"));
 // Routes
 // =============================================================
 require("./routes/html-routes.js")(app);
-// require("./routes/author-api-routes.js")(app);
+require("./routes/api-routes.js")(app);
 // require("./routes/post-api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
@@ -34,8 +34,4 @@ db.sequelize.sync({ force: true }).then(function() {
 	app.listen(PORT, function() {
 		console.log("App listening on PORT " + PORT);
 	});
-});
-
-app.listen(PORT, function() {
-	console.log("App listening on PORT " + PORT);
 });
