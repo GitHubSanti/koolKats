0a4e55db353ccd1b1d515e656bf17b8e740fe0f4
Spiderman <santiagosepulveda@me.com>
2019-01-24 20:14:16 -0500

    modified routing and acquisition of username and password to be sent to server

--- public/js/login.js
+++ public/js/login.js
@@ -1,16 +1,19 @@
-
 $("#login-btn").on("click", function(event) {
     event.preventDefault();
     let userCredentials = {};
     userCredentials.usernameInput = $("#inputUserName").val().trim();
     userCredentials.passwordInput = $("#inputPassword").val().trim();
 
-    $.post("/api/login", userCredentials, (serverRes => {
-          console.log(serverRes);
-          if (serverRes.length == 0 ) {
-            console.log("username and password combination doesn't exist");
-          } else if (serverRes.length > 0) {
-              console.log("You're logged in!");
-          }
-      }))
-  });
+    if (userCredentials.usernameInput == "" || userCredentials.passwordInput == "") {
+        alert("Please fill in both the username and password field.")
+    } else if (!userCredentials.usernameInput == "" && !userCredentials.passwordInput == "") {
+        $.post("/api/login", userCredentials, (serverRes => {
+            if (serverRes.length == 0) {
+                alert("username and password combination doesn't exist");
+            } else if (serverRes.length > 0) {
+                console.log("You're logged in!");
+                window.location.assign("/addEvent");
+            }
+        }))
+    }
+});
