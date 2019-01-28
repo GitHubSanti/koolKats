7f0bcddac257e2b74bdd10ffea38e99652318105
jittythekitty <jittythekitty@gmail.com>
2019-01-28 14:19:17 -0500

    added html and api routes to js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -13,7 +13,23 @@ module.exports = function(app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
-  app.get("/addevent", function(req, res) {
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
+
+  app.get("/addEvent", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/addEvent.html"));
+  });
+
+  app.get("/createLogin", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/createLogin.html"));
+  });
+
+  app.get("/:id/calendar", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/calendar.html"));
+  });
+
+  app.get("/about", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/about.html"));
   });
 };
