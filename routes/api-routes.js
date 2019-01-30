d5df232f3d95b9d599b82489bacfd502e53cde9a
Spiderman <santiagosepulveda@me.com>
2019-01-30 18:47:25 -0500

    new route added PUT for modifying current event

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -11,64 +11,81 @@ var db = require("../models");
 // Routes
 // =============================================================
 module.exports = function(app) {
-  // Creates new user
-  app.post("/api/createUser", function(req,res) {
-    db.Users.create({
-      username: req.body.usernameInput,
-      password: req.body.passwordInput
-    }).then(function(newUser){
-        res.json(newUser);
-    }).catch(function(err) {
-        // res.json("Unable to create new account due to:" + err)
-        res.json(err)
+    // POST route to create new user
+    app.post("/api/createUser", function(req, res) {
+        db.Users.create({
+            username: req.body.usernameInput,
+            password: req.body.passwordInput
+        }).then(function(newUser) {
+            res.json(newUser);
+        }).catch(function(err) {
+            // res.json("Unable to create new account due to:" + err)
+            res.json(err)
+        });
     });
-  });
 
-  // Logs in user
-  app.post("/api/login", function(req, res) {
-    db.Users.findAll({
-      where: {
-        username: req.body.usernameInput,
-        password: req.body.passwordInput
-      }
-    }).then(function(data) {
-      res.json(data);
+    // POST route to log user in
+    app.post("/api/login", function(req, res) {
+        db.Users.findAll({
+            where: {
+                username: req.body.usernameInput,
+                password: req.body.passwordInput
+            }
+        }).then(function(data) {
+            res.json(data);
+        });
     });
-  });
 
-  // Creates new Event
-  app.post("/api/addEvent/:id", function(req, res) {
-      db.Events.create({
-        title: req.body.eventTitle,
-        startDate: req.body.eventStartDate,
-        startTime: req.body.eventStartTime,
-        endTime: req.body.eventEndTime,
-        description: req.body.eventDescription,
-        UserId: req.params.id
-      }).then(function(createdEvent) {
-        res.json(createdEvent);
-      });
-  });
-
-  // Deletes Event
-app.delete("/api/deleteEvent/:id", function(req,res) {
-  db.Events.destroy({
-    where: {
-      id: req.params.id
-    }
-  }).then(function(DBresults) {
-    res.json(DBresults);
-  })
-});
-
-  // GET route to retrieve all the events and the users associated
-  app.get("/api/calendar/:id", function(req, res) {
-    db.Events.findAll({
-      where: {
-        UserId: req.params.id
-      }
-    }).then(function(allEvents) {
-      res.json(allEvents);
+    // GET route to retrieve all the events for user logged in
+    app.get("/api/calendar/:id", function(req, res) {
+        db.Events.findAll({
+            where: {
+                UserId: req.params.id
+            }
+        }).then(function(allEvents) {
+            res.json(allEvents);
+        });
     });
-  });
+
+    // POST route for creating new event
+    app.post("/api/addEvent/:id", function(req, res) {
+        db.Events.create({
+            title: req.body.eventTitle,
+            startDate: req.body.eventStartDate,
+            startTime: req.body.eventStartTime,
+            endTime: req.body.eventEndTime,
+            description: req.body.eventDescription,
+            UserId: req.params.id
+        }).then(function(createdEvent) {
+            res.json(createdEvent);
+        });
+    });
+
+    // PUT route for updating specificed event
+    app.put("/api/modifyEvent/:id", function(req, res) {
+        db.Events.update({
+          title: req.body.eventTitle,
+          startDate: req.body.eventStartDate,
+          startTime: req.body.eventStartTime,
+          endTime: req.body.eventEndTime,
+          description: req.body.eventDescription
+        }, {
+            where: {
+                id: req.params.id
+            }
+        }).then(function(modifiedEvent) {
+            res.json(modifiedEvent);
+        });
+    });
+
+    // DELETE route for deleting specificed event
+    app.delete("/api/deleteEvent/:id", function(req, res) {
+        db.Events.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(function(DBresults) {
+            res.json(DBresults);
+        })
+    });
 };
